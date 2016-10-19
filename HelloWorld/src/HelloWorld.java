b14a3f17021d55dc850171a52c0d55d878738233
mr-ma <mohsen.ahmadv@gmail.com>
2016-10-19 17:09:15 +0200

    init repo

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author Mohsen
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello ASE2016 - how are you? :)");
+	}
+
+}
