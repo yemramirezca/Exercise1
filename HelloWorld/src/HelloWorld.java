b890265a7654084643f279f75eddf46f61fdf4fe
yesika ramirez <yesika.ramirez@tum.de>
2016-10-26 02:15:51 +0200

    HelloWorld class modified

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -12,7 +12,7 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello ASE2016 - how are you? :)");
+		System.out.println("Hello ASE2016 - how are you now? :)");
 	}
 
 }
